d94af8ea6a97020df9022aaa3dba551efef6a282
simtape <s.cassetta1@studenti.unimol.it>
2021-01-06 20:01:15 +0100

    modifiche porta

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const postRoute = require('./routes/posts');
 const userRoute = require('./routes/users');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -16,4 +17,4 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, ()=>
                 console.log('connected to db!')
                 );
 
-app.listen(3000);
+app.listen(PORT);
