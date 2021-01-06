3ee8a3634fd4e5f526d3a4dd210df41613c9be16
simtape <s.cassetta1@studenti.unimol.it>
2021-01-06 20:08:02 +0100

    altre modifiche sempre per risolvere errore heroku

--- app.js
+++ app.js
@@ -17,4 +17,11 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, ()=>
                 console.log('connected to db!')
                 );
 
-                app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
