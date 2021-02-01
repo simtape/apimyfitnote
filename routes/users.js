9f0e2e36bc99192cf593faf5c87746c1e87214be
simtape <s.cassetta1@studenti.unimol.it>
2021-02-01 17:07:25 +0100

    d

--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.post('/registrazione', async (req, res) => {
         user.save()
             .then(user => {
                 res.json({
-                    message: 'Utente registrato con successo!'
+                    message: user
                 })
             })
             .catch(error => {
