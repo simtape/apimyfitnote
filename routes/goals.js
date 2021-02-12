f4b3de0fb762346c22ae3a412241a85a95d191a2
simtape <s.cassetta1@studenti.unimol.it>
2021-02-12 12:20:42 +0100

    modifiche goal 2

--- routes/goals.js
+++ routes/goals.js
@@ -4,13 +4,31 @@ const Goal = require('../models/Goal');
 
 
 router.post('/create_goals', async (req, res) => {
+
+
     const goal = new Goal({
         name: req.body.name,
         obiettivo: req.body.obiettivo,
         valore_attuale: req.body.valore_attuale,
         id_user: req.body.id_user
     })
-    Goal.updateOne({id_user: req.body.id_user}, {$push:{name: goal.name}})
+    Goal.find({ id_user: req.body.id_user }).count(function (err, number) {
+        if (number != 0) {
+            goal.save().then(goal => {
+                res.json({
+                    goal
+                })
+
+            });
+
+        } else {
+
+            Goal.updateOne({ id_user: req.body.id_user }, { $push: { name: goal.name } })
+        }
+
+
+    })
+
 
 
 });
