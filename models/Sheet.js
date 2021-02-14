21e936fde563c59cdd70354fd4eb5a7c01e3f667
simtape <s.cassetta1@studenti.unimol.it>
2021-02-15 00:51:17 +0100

    string

--- models/Sheet.js
+++ models/Sheet.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const SheetSchema = mongoose.Schema({
     exercises: [String],
     days: [String],
-    reps: [Number],
-    series: [Number],
+    reps: [String],
+    series: [String],
     date: { type: Date, default: Date.now },
     name_sheet: { type: String, required: true, unique: true },
     user_id: String
